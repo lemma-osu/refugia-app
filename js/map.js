77bec06f66e55f450b6ef3c2e20756e883591f89
Matt Gregory <matt.gregory@oregonstate.edu>
2021-06-02 14:16:25 -0700

    Add initial map events (zoom, dblclick)

--- js/map.js
+++ js/map.js
@@ -1,8 +1,12 @@
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/dark-v10",
-  bounds: [[-125.0, 37.0], [-120.0, 49.5]]
+  // bounds: [[-125.0, 37.0], [-120.0, 49.5]]
+  bounds: [[-123.0, 43.5], [-122.85, 43.65]]
 });
+const modal_div = document.querySelector("#example-modal");
+const modal = new bootstrap.Modal(modal_div);
+const zoom_limit = 11.5;
 let current_layer;
 
 function change_map(map, layer_definition, current_layer) {
@@ -25,9 +29,33 @@ function change_map(map, layer_definition, current_layer) {
   return layer_definition.layer.id;
 }
 
+function display_modal(event) {
+  const coord_div = modal_div.querySelector("#coord");
+  coord_div.textContent = `Coordinate: (${event.lngLat.lat.toFixed(3)}, ${event.lngLat.lng.toFixed(3)})`;
+  modal.show();
+}
+
+map.on("dblclick", function(event) {
+  if (map.getZoom() >= zoom_limit) {
+    event.preventDefault();
+    display_modal(event);
+  }
+})
+
+function handle_cursor() {
+  map.getCanvas().style.cursor = map.getZoom() >= zoom_limit
+    ? "crosshair"
+    : "grab";
+}
+
+map.on("zoom", function() {
+  handle_cursor();
+});
+
 function initialize() {
   create_source_objects(function () {
     current_layer = change_map(map, map_layers["ogsi-80"], current_layer);
+    handle_cursor();
   });
 }
 
