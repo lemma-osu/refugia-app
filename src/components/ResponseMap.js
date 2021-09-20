5654416adb6823a3ca7bff3b58d06870dd5b78d2
Matt Gregory <matt.gregory@oregonstate.edu>
2021-09-20 14:24:36 -0700

    Add comments to ResponseMap component

--- src/components/ResponseMap.js
+++ src/components/ResponseMap.js
@@ -3,11 +3,21 @@ import DefaultMap from "./Map/DefaultMap";
 import CustomMultiLayerMap from "./Map/CustomMultiLayerMap";
 import SwipeMap from "./Map/SwipeMap";
 
+// Constants for box sizing
 const METERS_PER_PIXEL = 30.0;
 const LAT_DEGREES_PER_PIXEL = METERS_PER_PIXEL / 111139.0;
 const LNG_DEGREES_PER_PIXEL = (lat) =>
   METERS_PER_PIXEL / (-12.5086 * lat * lat - 173.5626 * lat + 112246);
 
+// Maximum zoom for map component
+const MAX_ZOOM = 12.9;
+
+/**
+ * Create the box based on coordinate and offset
+ * @param {Object} coord - The coordinate at the center of the box
+ * @param {Object} offset - Offsets for latitude and longitude
+ * @returns {float[][][]}
+ */
 function makeBox(coord, offset) {
   return [
     [
@@ -20,14 +30,28 @@ function makeBox(coord, offset) {
   ];
 }
 
-const getOffset = (lat, miwSize) => {
+/**
+ * Get an offset based on the size of the MIW and the
+ * current latitude.  The longitude offset will change as
+ * as function of latitude
+ * @param {float} lat - The current latitude
+ * @param {float[]} miwSize - The requested miwSize in pixels
+ * @returns {Object}
+ */
+function getOffset(lat, miwSize) {
   return {
     lng: miwSize[0] * LNG_DEGREES_PER_PIXEL(lat),
     lat: miwSize[1] * LAT_DEGREES_PER_PIXEL,
   };
-};
+}
 
-const initializeGeojson = (coord, offset) => {
+/**
+ * Create an initial GeoJSON object from coordinate and offset
+ * @param {Object} coord - The coordinate at the center of the box
+ * @param {Object} offset - Offsets for latitude and longitude
+ * @returns {Object}
+ */
+function initializeGeojson(coord, offset) {
   return {
     type: "FeatureCollection",
     features: [
@@ -40,8 +64,15 @@ const initializeGeojson = (coord, offset) => {
       },
     ],
   };
-};
+}
 
+/**
+ * Initialize state for this component
+ * @param {Object} obj - Container object
+ * @param {Object} obj.config - Model configuration
+ * @param {float[][]} obj.miwSize - Initial MIW size
+ * @returns {Object}
+ */
 function initialize({ config, miwSize }) {
   const coord = { lng: config.initial_lng, lat: config.initial_lat };
   const offset = getOffset(coord.lat, miwSize);
@@ -53,6 +84,14 @@ function initialize({ config, miwSize }) {
   };
 }
 
+/**
+ * Reducer function for setting state
+ * @param {Object} state - Current state
+ * @param {Object} action - Current action to perform
+ * @param {string} action.type - Name of the current action
+ * @param {Object} action.payload - Current payload to set object
+ * @returns {Object}
+ */
 function reducer(state, action) {
   switch (action.type) {
     case "SET_COORD":
@@ -79,13 +118,15 @@ export default function ResponseMap({
   onMouseMove,
   onClickedCoord,
 }) {
+  // Reference to map
   const [map, setMap] = useState(null);
+
+  // Set up reducer and initial state
   const [state, dispatch] = useReducer(
     reducer,
     { config, miwSize },
     initialize
   );
-  const maxZoom = 12.9;
 
   function renderBox(coord) {
     if (!map || !map.getSource("box")) return;
@@ -100,6 +141,7 @@ export default function ResponseMap({
     if (!map) return;
     const canvas = map.getCanvasContainer();
 
+    // Grab and move the MIW
     function onMove(e) {
       const coord = e.lngLat;
       canvas.style.cursor = "grabbing";
@@ -110,11 +152,14 @@ export default function ResponseMap({
       renderBox(coord);
     }
 
+    // On mouse released, turn off onMove event
     function onUp(e) {
       canvas.style.cursor = "";
       map.off("mousemove", onMove);
     }
 
+    // On mouse press, associate the two event handlers and
+    // change cursor to grabbing icon
     function onDown(e) {
       e.preventDefault();
       canvas.style.cursor = "grab";
@@ -131,43 +176,27 @@ export default function ResponseMap({
     map.on("mousedown", "box", onDown);
   }
 
+  // Set the map reference once the maps have loaded
   function handleMapLoad(compareMaps) {
     setMap(compareMaps.left);
   }
 
+  // Change the box offsets if the MIW size changes
   useEffect(() => {
     dispatch({ type: "SET_OFFSET", payload: miwSize });
   }, [miwSize]);
 
+  // Rerender box
   useEffect(() => {
     renderBox(state.coord);
     setMapHandlers();
   }, [state.offset]);
 
+  // Add event handler to report mouse location
   useEffect(() => {
     if (!map) return;
     map.on("mousemove", onMouseMove);
   }, [map, onMouseMove]);
-
-  // useEffect(() => {
-  //   if (!map) return;
-
-  //   function handleCursor() {
-  //     map.getCanvas().style.cursor =
-  //       map.getZoom() >= zoomCursorSwitch ? "crosshair" : "grab";
-  //   }
-
-  //   function handleDblClick(event) {
-  //     if (map.getZoom() >= zoomCursorSwitch) {
-  //       event.preventDefault();
-  //       map.getCanvas().style.cursor = "wait";
-  //       onClickedCoord(event.lngLat);
-  //     }
-  //   }
-
-  //   map.on("zoomend", handleCursor);
-  //   map.on("dblclick", handleDblClick);
-  // }, [map, onClickedCoord]);
 
   useEffect(() => {
     if (!map) return;
@@ -225,7 +254,7 @@ export default function ResponseMap({
             lng: config.initial_lng,
             lat: config.initial_lat,
             zoom: config.initial_zoom,
-            maxZoom: maxZoom,
+            maxZoom: MAX_ZOOM,
           }}
           customLayers={config.tiles}
           layerIdx={idx}
@@ -238,7 +267,7 @@ export default function ResponseMap({
             lng: config.initial_lng,
             lat: config.initial_lat,
             zoom: config.initial_zoom,
-            maxZoom: maxZoom,
+            maxZoom: MAX_ZOOM,
           }}
         />
       }
